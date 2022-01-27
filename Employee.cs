8da1d617aee01cf16d9cef1eaf5f1948022966b3
1000007201 <nishant.sharma5504@gmail.com>
2022-01-27 22:43:45 +0530

    [Nishant] Add:Part time wage calculation done

--- Employee.cs
+++ Employee.cs
@@ -11,16 +11,21 @@ namespace EmployeeWageComputation
         public void CalculateWage()
         {
             Random random = new Random();
-            const int PRESENT = 1, ABSENT = 0, WAGE_PER_HR = 20;
+            const int PRESENT = 1, ABSENT = 0, WAGE_PER_HR = 20, PART_TIME = 2;
             int workinghour = 0;
-            if (random.Next(2) == PRESENT)
+            if (random.Next(3) == PRESENT)
             {
                 Console.WriteLine("Employee is Present");
-                workinghour = 8;
+                workinghour = 12;
             }
-            if (random.Next(2) == ABSENT)
+            if (random.Next(3) == ABSENT)
             {
                 Console.WriteLine("Employee is present");
+            }
+            if (random.Next(3) == PART_TIME)
+            {
+                Console.WriteLine("Employee is present part time");
+                workinghour = 8;
             }
             int wage = workinghour * WAGE_PER_HR;
             Console.WriteLine("Employee daily wage is {0}", wage);
