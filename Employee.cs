667b2b7567e33f85c51b9667e0c45d67ca63f83f
1000007201 <nishant.sharma5504@gmail.com>
2022-01-27 23:12:29 +0530

    [Nishant] Add:Monthly wages calculation done

--- Employee.cs
+++ Employee.cs
@@ -11,25 +11,31 @@ namespace EmployeeWageComputation
         public void CalculateWage()
         {
             Random random = new Random();
-            const int PRESENT = 1, ABSENT = 0, WAGE_PER_HR = 20, PART_TIME = 2;
+            const int PRESENT = 1, ABSENT = 0, WAGE_PER_HR = 20, PART_TIME = 2, WORKING_DAYS = 20;
             int workinghour = 0;
-            int empstatus = random.Next(3);
-            switch (empstatus)
+            int totalwage = 0;
+            for (int i = 0; i < WORKING_DAYS; i++)
             {
-                case PRESENT:
-                    Console.WriteLine("Employee is Present");
-                    workinghour = 12;
-                    break;
-                case ABSENT:
-                    Console.WriteLine("Employee is present");
-                    break;
-                case PART_TIME:
-                    Console.WriteLine("Employee is present part time");
-                    workinghour = 8;
-                    break;
+                int empstatus = random.Next(3);
+                switch (empstatus)
+                {
+                    case ABSENT:
+                        Console.WriteLine("Employee is Absent");
+                        break;
+                    case PRESENT:
+                        Console.WriteLine("Employee is Present");
+                        workinghour = 12;
+                        break;
+
+                    case PART_TIME:
+                        Console.WriteLine("Employee is present part time");
+                        workinghour = 8;
+                        break;
+                }
+                int wage = workinghour * WAGE_PER_HR;
+                totalwage += wage;
             }
-            int wage = workinghour * WAGE_PER_HR;
-            Console.WriteLine("Employee daily wage is {0}", wage);
+            Console.WriteLine("Employee Monthly wage is {0}", totalwage);
 
         }
     }
