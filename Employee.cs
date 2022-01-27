992c5d584b6522c14f04a7c7588fa8c8e75b967e
1000007201 <nishant.sharma5504@gmail.com>
2022-01-27 23:31:34 +0530

    [Nishant] Add:Limited working hour wage calculation done

--- Employee.cs
+++ Employee.cs
@@ -11,29 +11,31 @@ namespace EmployeeWageComputation
         public void CalculateWage()
         {
             Random random = new Random();
-            const int PRESENT = 1, ABSENT = 0, WAGE_PER_HR = 20, PART_TIME = 2, WORKING_DAYS = 20;
+            const int PRESENT = 1, ABSENT = 0, WAGE_PER_HR = 20, PART_TIME = 2, WORKING_DAYS = 20, MAX_WRK_HR = 100;
             int workinghour = 0;
             int totalwage = 0;
-            for (int i = 0; i < WORKING_DAYS; i++)
+            int totalworkinghour = 0;
+            Console.WriteLine("{0} {1} {2} {3}", "DAY", "WORKING_HOUR", "WAGE", "TOTAL_WRK_HR");
+            for (int day = 0; day < WORKING_DAYS && totalworkinghour < MAX_WRK_HR; day++)
             {
                 int empstatus = random.Next(3);
                 switch (empstatus)
                 {
                     case ABSENT:
-                        Console.WriteLine("Employee is Absent");
+                        workinghour = 0;
                         break;
                     case PRESENT:
-                        Console.WriteLine("Employee is Present");
                         workinghour = 12;
                         break;
 
                     case PART_TIME:
-                        Console.WriteLine("Employee is present part time");
                         workinghour = 8;
                         break;
                 }
                 int wage = workinghour * WAGE_PER_HR;
                 totalwage += wage;
+                totalworkinghour += workinghour;
+                Console.WriteLine("{0} {1} {2} {3}",day,workinghour,wage,totalworkinghour);
             }
             Console.WriteLine("Employee Monthly wage is {0}", totalwage);
 
